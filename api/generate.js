5f654689bf81048426e77a268eba11fea61f106f
concept-starte <hcghywqvqy@privaterelay.appleid.com>
2026-01-11 15:44:28 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,3 +1,6 @@
+export const config = {
+  runtime: "nodejs"
+};
 export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).send("Method Not Allowed");
